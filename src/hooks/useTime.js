cc455469ce2452f24c663db28bce4200b265d91e
chito <chito.pepsi@gmail.com>
2024-05-11 18:25:40 -0600

    tarea reloj

---
+++ src/hooks/useTime.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+export function useWorldTimeAPI(timezone) {
+  const [time, setTime] = useState("");
+  const [day, setDay] = useState("");
+  const [dayYear, setDayYear] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://worldtimeapi.org/api/timezone/${timezone}`);
+        const data = await response.json();
+        setTime(data.datetime);
+        setDay(data.day_of_week);
+        setDayYear(data.utc_datetime);
+      } catch (error) {
+        console.error("Error fetching time:", error);
+      }
+    };
+
+    fetchData();
+
+    const intervalId = setInterval(fetchData, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [timezone]);
+
+  return { time, day, dayYear };
+}
